17f8130621ca53ca6d35ffd457ac87195265c5ae
jamshid <jamshidalimov3464@gmail.com>
2025-07-22 17:27:10 +0500

    Java misollari

---
+++ src/Main.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+//        System.out.print("Birinchi sonni kiriting: ");
+//        int son1 = scanner.nextInt();
+
+//        System.out.print("Ikkinchi sonni kiriting: ");
+//        int son2 = scanner.nextInt();
+//
+//        System.out.print("Uchinchi sonni kiriting: ");
+//        int son3 = scanner.nextInt();
+//
+//        int musbatSoni = 0;
+//
+//
+//        if (son1 > 0) musbatSoni++;
+//        if (son2 > 0) musbatSoni++;
+//        if (son3 > 0) musbatSoni++;
+//
+//
+//        System.out.println("Musbat sonlar soni: " + musbatSoni);
+
+//        System.out.print("Birinchi sonni kiriting: ");
+//        int son1 = scanner.nextInt();
+//
+//        System.out.print("Ikkinchi sonni kiriting: ");
+//        int son2 = scanner.nextInt();
+//
+//
+//        if (son1 > son2) {
+//            System.out.println("Natija: " + son1 + " " + son2);
+//        } else {
+//            System.out.println("Natija: " + son2 + " " + son1);
+//        }
+
+
+//        System.out.print("1 dan 999 gacha bo'lgan sonni kiriting: ");
+//        int son = scanner.nextInt();
+//
+//
+//        if (son < 1 || son > 999) {
+//            System.out.println("Xatolik: Son 1 dan 999 gacha bo'lishi kerak!");
+//        } else {
+//            String xonalik = "";
+//            String juftToq = "";
+//
+//
+//            if (son < 10) {
+//                xonalik = "bir xonali";
+//            } else if (son < 100) {
+//                xonalik = "ikki xonali";
+//            } else {
+//                xonalik = "uch xonali";
+//            }
+//
+//
+//            if (son % 2 == 0) {
+//                juftToq = "juft son";
+//            } else {
+//                juftToq = "toq son";
+//            }
+//
+//
+//            System.out.println("Natija: " + xonalik + " " + juftToq);
+//        }
+
+
+//        System.out.print("Birinchi sonni kiriting: ");
+//        int a = scanner.nextInt();
+//
+//        System.out.print("Ikkinchi sonni kiriting: ");
+//        int b = scanner.nextInt();
+//
+//        System.out.print("Uchinchi sonni kiriting: ");
+//        int c = scanner.nextInt();
+//
+//        int ortancha;
+//
+//
+//        if ((a > b && a < c) || (a < b && a > c)) {
+//            ortancha = a;
+//        } else if ((b > a && b < c) || (b < a && b > c)) {
+//            ortancha = b;
+//        } else {
+//            ortancha = c;
+//        }
+//        System.out.println("O'rtanchasi: " + ortancha);
+
+
+
+
+
+    }
+}
